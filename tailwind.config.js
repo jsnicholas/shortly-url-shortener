626b1b68ff413c15d4427c283ee0701c1c148b6b
Nick <harmoniacodes@proton.me>
2023-05-16 08:04:43 -0400

    migrate to vite and typescript

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [],
   theme: {
-    colors: {
-      cyan: "hsl(180, 66%, 49%)",
-      darkviolet: "hsl(257, 27%, 26%)",
-      /* Secondary Colors */
-      red: "hsl(0, 87%, 67%)",
-      /* Neutral Colors */
-      gray: "hsl(0, 0%, 60%)",
-      infobg: "hsl(230, 2%, 96%)",
-      grayviolet: "hsl(257, 7%, 63%)",
-      darkblue: "hsl(255, 11%, 22%)",
-      verydarkviolet: "hsl(260, 8%, 14%)",
-      white: "#FFFFFF",
-    },
     extend: {},
   },
   plugins: [],
-};
+}
+
